413b12fd744b45fb1d8924f34622f843a8e09a81
gk1591998 <golang1507@gmail.com>
2024-12-30 02:54:07 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+
+	number := 5
+	fmt.Printf("Factorial of %d is %d\n", number, factorial(number))
+
+	// ...existing code...
+}
